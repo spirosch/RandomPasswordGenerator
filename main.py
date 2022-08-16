f1adbddf0ca6878a5eec2905f24c15692c284a6a
Spiros <geokh@hotmail.com>
2022-08-16 21:49:36 +0300

    Reduce code lines and use secrets module

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Password Generator Project
 import random
-
+import secrets
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -12,40 +12,61 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-random_letters = random.sample(letters, nr_letters)
-random_letter_string = str(random_letters)
-final_letters = random_letter_string.replace(' ', '')\
-    .replace('[]', '')\
-    .replace(',', '')\
-    .replace("'", '')\
-    .strip('[]')
+password_list = []
 
-random_numbers = random.sample(numbers, nr_numbers)
-random_number_string = str(random_numbers)
-final_number = random_number_string.replace(' ', '')\
-    .replace('[]', '')\
-    .replace(',', '')\
-    .replace("'", '')\
-    .strip('[]')
+for char in range(1, nr_letters + 1):
+  password_list.append(secrets.choice(letters))
 
-random_symbol = random.sample(symbols, nr_symbols)
-random_symbol_string = str(random_symbol)
-final_symbol = random_symbol_string.replace(' ', '')\
-    .replace('[]', '')\
-    .replace(',', '')\
-    .replace("'", '')\
-    .strip('[]')
+for char in range(1, nr_symbols + 1):
+  password_list += secrets.choice(symbols)
 
-combined_password = final_letters, final_number, final_symbol
-final_password_string = str(combined_password)
-final_password = final_password_string.replace(' ', '')\
-    .replace('[]', '')\
-    .replace(',', '')\
-    .replace("'", '')\
-    .strip('[]')\
-    .strip('()')
-combined_list_password = list(combined_password)
+for char in range(1, nr_numbers + 1):
+  password_list += secrets.choice(numbers)
 
-shuffled_password = ''.join(random.sample(final_password, len(final_password)))
+random.shuffle(password_list)
 
-print(f"Your password is: {shuffled_password}")
+final_password = ""
+for char in password_list:
+  final_password += char
+
+print(f"Your password is: {final_password}")
+
+#Below is the first try if you want to see a different approach
+
+# random_letters = random.sample(letters, nr_letters)
+# random_letter_string = str(random_letters)
+# final_letters = random_letter_string.replace(' ', '')\
+#     .replace('[]', '')\
+#     .replace(',', '')\
+#     .replace("'", '')\
+#     .strip('[]')
+
+# random_numbers = random.sample(numbers, nr_numbers)
+# random_number_string = str(random_numbers)
+# final_number = random_number_string.replace(' ', '')\
+#     .replace('[]', '')\
+#     .replace(',', '')\
+#     .replace("'", '')\
+#     .strip('[]')
+
+# random_symbol = random.sample(symbols, nr_symbols)
+# random_symbol_string = str(random_symbol)
+# final_symbol = random_symbol_string.replace(' ', '')\
+#     .replace('[]', '')\
+#     .replace(',', '')\
+#     .replace("'", '')\
+#     .strip('[]')
+
+# combined_password = final_letters, final_number, final_symbol
+# final_password_string = str(combined_password)
+# final_password = final_password_string.replace(' ', '')\
+#     .replace('[]', '')\
+#     .replace(',', '')\
+#     .replace("'", '')\
+#     .strip('[]')\
+#     .strip('()')
+# combined_list_password = list(combined_password)
+
+# shuffled_password = ''.join(random.sample(final_password, len(final_password)))
+
+# print(f"Your password is: {shuffled_password}")
